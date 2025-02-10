efa5b1d1bcfcf7d88189aa1aeff09dadd03c3887
shivam <shivam.singhal.211@gmail.com>
2025-02-10 23:12:32 +0530

    changes in appwrite config file

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -2,6 +2,9 @@ import { Client, Account } from "appwrite";
 
 const client = new Client();
 
+console.log("Appwrite Endpoint:", import.meta.env.VITE_APPWRITE_URL);
+console.log("Appwrite Project ID:", import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
 
 client
   .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
